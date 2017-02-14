52ba232dc0a25d02a87fc81b2a832b84fa66515b
Catouse <catouse@gmail.com>
2017-02-14 17:50:46 +0800

    * remove cheerio package.

--- app/views/components/draft-editor.js
+++ app/views/components/draft-editor.js
@@ -1,6 +1,5 @@
 import React, {Component}            from 'react';
 import ReactDOM                      from 'react-dom';
-import Cheerio                       from 'cheerio';
 import {
     Editor,
     EditorState,
