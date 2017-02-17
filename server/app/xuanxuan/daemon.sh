1ebd2e3d01940c906416ad59d3a8fa9c328bf638
liugang <liugang@cnezsoft.com>
2017-02-17 10:30:40 +0800

    * Adjust codes.

--- server/app/xuanxuan/daemon.sh
+++ server/app/xuanxuan/daemon.sh
@@ -4,7 +4,8 @@
 #
 # @author Gang Liu <liugang@cnezsoft.com>
 #
+username='www-data'  # Use the run user of your web server as username.
 running=`ps aux|grep 'php ./server.php'|awk '$1=="www-data"{print $0}'`
 if [ ! "$running" ]; then
-    nohup sudo sudo -u www-data ./server.php 2>1&
+    nohup sudo sudo -u $username ./server.php 2>1&
 fi
